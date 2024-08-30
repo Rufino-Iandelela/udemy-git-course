a5747df4ce6f4a4938b9c1d99a2703b305588fb8
Cp9-30 <rufinoiadelela@gmail.com>
2024-08-30 21:16:21 +0100

    enviar meu segundo projecto no git

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+
+        String st = "You can be better.";
+
+        System.out.print("Hello and welcome" + st);
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
